fd5679feb7e13b876c68c6d891c41af64f432f22
mari <mnepach@gmail.com>
2025-10-22 09:52:10 +0300

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    classpath 'com.google.gms:google-services:4.4.4'
+    classpath ("com.google.gms:google-services:4.4.4")
 }
 
 tasks.test {
